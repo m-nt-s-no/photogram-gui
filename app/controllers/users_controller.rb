053a0320bee5494d4af7a5fbaa28cdd4dca2da71
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2024-12-10 02:44:58 +0000

    added RCAV for user index, showing user, showing user's photos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,40 @@
 class UsersController < ApplicationController
   def index
+    @list_of_users = User.all.order({ :created_at => :desc })
+    render({ :template => "user_templates/index" })
   end
 
   def show
+    user_name = params.fetch("username")
+    @user = User.where({:username => user_name }).at(0)
+    render({ :template => "user_templates/show" })
+  end
+
+  def insert
+    @new_user = User.new
+    @new_user.username = params.fetch("username")
+
+    if @new_user.valid?
+      @new_user.save
+      redirect_to("/users/#{@new_user.username}")
+    else
+      same_name = User.where({:username => @new_user.username }).at(0)
+      redirect_to("/users/#{same_name.username}")
+    end
+  end
+
+  def update
+    the_id = params.fetch("userid")
+    new_name = params.fetch("new_username")
+    user_to_be_updated = User.find(the_id)
+    user_to_be_updated.username = new_name
+
+    if user_to_be_updated.valid?
+      user_to_be_updated.save
+      redirect_to("/users/#{user_to_be_updated.username}")
+    else
+      same_name = User.where({:username => new_name}).at(0)
+      redirect_to("/users/#{same_name.username}")
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get("/", {:controller => "users", :action => "index"})
   get("/users", {:controller => "users", :action => "index"})
   get("/users/:username", {:controller => "users", :action => "show"})
+  post("/insert_user", {:controller => "users", :action => "insert"})
+  post("/update_user/:username", { :controller => "users", :action => "update" })
 
   get("/photos", {:controller => "photos", :action => "index"})
   get("/photos/:photo_id", {:controller => "photos", :action => "index"})
