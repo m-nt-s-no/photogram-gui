cc9e870b26b1a8d03592355a1f7e4c34625903dc
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2024-12-11 03:18:06 +0000

    added update, delete, and add comment methods to photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -11,9 +11,25 @@ class PhotosController < ApplicationController
   end
 
   def update
+    the_id = params.fetch("id")
+    new_image = params.fetch("new_image")
+    new_caption = params.fetch("new_caption")
+
+    the_photo = Photo.find(the_id)
+    the_photo.image = new_image
+    the_photo.caption = new_caption
+    the_photo.updated_at = Time.current
+
+    the_photo.save
+    redirect_to("/photos/#{the_photo.id}")
   end
 
   def delete
+    the_id = params.fetch("id")
+    the_photo = Photo.find(the_id)
+    the_photo.destroy
+
+    redirect_to("/photos")
   end
 
   def insert_photo
@@ -31,5 +47,18 @@ class PhotosController < ApplicationController
   end
 
   def insert_comment
+    @new_comment = Comment.new
+    @new_comment.photo_id = params.fetch("photo_id").to_i
+    @new_comment.author_id = params.fetch("author_id").to_i
+    @new_comment.body = params.fetch("comment")
+
+    if @new_comment.valid?
+      @new_comment.save
+      the_photo = Photo.find(@new_comment.photo_id)
+      the_photo.comments_count = the_photo.comments_count + 1
+      the_photo.save
+    end
+    
+    redirect_to("/photos/#{@new_comment.photo_id}")
   end
 end
